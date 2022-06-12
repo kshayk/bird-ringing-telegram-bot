3f4f2f3c1bd84ffd7702449d0634b6a93752fc81
kshay <kshayk0@gmail.com>
2022-06-12 18:54:26 +0300

    Improved lists code

--- src/commands/BirdCommand.ts
+++ src/commands/BirdCommand.ts
@@ -2,6 +2,7 @@ import {ICommand} from "./ICommand";
 import TelegramBot from "../TelegramBot";
 import * as process from "process";
 import * as fs from "fs";
+import TextFilesUtil from "../Util/TextFilesUtil";
 
 class BirdCommand implements ICommand {
     private static letterSegments = [
@@ -14,9 +15,6 @@ class BirdCommand implements ICommand {
 
     async execute(requestData: any): Promise<void> {
         let messageArray = requestData.message.text.split(" ");
-
-        //get root path of project
-        let birdsPath = process.cwd() + '/birds';
 
         if (messageArray.length === 1) {
             await BirdCommand.sendFullBirdListOptions(requestData);
@@ -32,7 +30,7 @@ class BirdCommand implements ICommand {
             let segmentedFiles = BirdCommand.getSegmentedFiles();
 
             if (segmentedFiles[+messageArray[2] - 1]) {
-                let birdsList = BirdCommand.generateBirdList(segmentedFiles[messageArray[2] - 1], 3);
+                let birdsList = TextFilesUtil.generateFilesList(segmentedFiles[messageArray[2] - 1], 'ציפור', 3);
                 await TelegramBot.sendMessage(requestData.message.chat.id, "יש לבחור את הציפור המבוקשת מבין הרשימה.", birdsList);
                 return;
             }
@@ -45,7 +43,7 @@ class BirdCommand implements ICommand {
 
         let birdName = messageArray.join("-");
 
-        let files = fs.readdirSync(birdsPath).filter(file => file.startsWith(birdName)).sort((a, b) => {
+        let files = fs.readdirSync(BirdCommand.birdsPath).filter(file => file.startsWith(birdName)).sort((a, b) => {
             // sort by file name
             return a.localeCompare(b);
         });
@@ -57,31 +55,15 @@ class BirdCommand implements ICommand {
 
         if (files.length === 1) {
             // get contents of the file
-            let fileContents = fs.readFileSync(birdsPath + '/' + files[0], 'utf8');
+            let fileContents = fs.readFileSync(BirdCommand.birdsPath + '/' + files[0], 'utf8');
             await TelegramBot.sendMessage(requestData.message.chat.id, fileContents);
             return;
         }
 
         // if there are more than one file, send a list of the files
-        const birdsList = BirdCommand.generateBirdList(files, 1);
+        const birdsList = TextFilesUtil.generateFilesList(files, 'ציפור',1);
 
         await TelegramBot.sendMessage(requestData.message.chat.id, "אנא בחרו באחת מהאפשרויות", birdsList);
-        return;
-    }
-
-    private static generateBirdList(birdFileNames : string[], itemsInRow : number = 1): any[] {
-        let result = [];
-
-        for (let i = 0; i < birdFileNames.length; i++) {
-            const fileName = "ציפור " + birdFileNames[i].replace(/-/g, " ").replace('.txt', '');
-            if (i % itemsInRow === 0) {
-                result.push([{text: fileName}]);
-            } else {
-                result[result.length - 1].push({text: fileName});
-            }
-        }
-
-        return result;
     }
 
     private static breakListIntoSegments(birdFileNames : string[]) : any[] {
--- src/commands/TermCommand.ts
+++ src/commands/TermCommand.ts
@@ -14,6 +14,29 @@ class TermCommand implements ICommand {
             await TermCommand.sendAvailableTerms(requestData);
             return;
         }
+
+        messageArray.shift();
+
+        let termName = messageArray.join("-");
+
+        let files = fs.readdirSync(TermCommand.termsPath).filter(file => file.startsWith(termName)).sort((a, b) => {
+            // sort by file name
+            return a.localeCompare(b);
+        });
+
+        if (files.length === 0) {
+            await TelegramBot.sendMessage(requestData.message.chat.id, "המונח המבוקש לא נמצא");
+            return;
+        }
+
+        if (files.length === 1) {
+            // get contents of the file
+            let fileContents = fs.readFileSync(TermCommand.termsPath + '/' + files[0], 'utf8');
+            await TelegramBot.sendMessage(requestData.message.chat.id, fileContents);
+            return;
+        }
+
+        await TelegramBot.sendMessage(requestData.message.chat.id, "אנא בחרו באחת מהאפשרויות", TextFilesUtil.generateFilesList(files, 'מונח', 3));
     }
 
     private static async sendAvailableTerms(requestData: any): Promise<void> {
